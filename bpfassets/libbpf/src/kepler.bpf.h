be0971743823e047acfda18e66a2ed853b5771f5
Dave Tucker <dave@dtucker.co.uk>
2024-04-26 15:52:48 +0100

    wip: no vmlinux
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- bpfassets/libbpf/src/kepler.bpf.h
+++ bpfassets/libbpf/src/kepler.bpf.h
@@ -14,32 +14,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#ifndef __KEPLER_H__
+#define __KEPLER_H__
+
+#include <linux/bpf.h>
+#include <linux/types.h>
+
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
 /* In Linux 5.4 asm_inline was introduced, but it's not supported by clang.
  * Redefine it to just asm to enable successful compilation.
- * see https://github.com/iovisor/bcc/commit/2d1497cde1cc9835f759a707b42dea83bee378b8 for more details
+ * see
+ * https://github.com/iovisor/bcc/commit/2d1497cde1cc9835f759a707b42dea83bee378b8
+ * for more details
  */
-#if defined(__TARGET_ARCH_x86)
-#include "vmlinux-x86.h"
-#endif
-#if defined(__TARGET_ARCH_arm64)
-#include "vmlinux-arm64.h"
-#endif
-#if defined(__TARGET_ARCH_s390)
-#include "vmlinux-s390.h"
-#endif
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_core_read.h>
-#include <bpf/bpf_tracing.h>
-
 #ifdef asm_inline
 #undef asm_inline
 #define asm_inline asm
 #endif
-
-typedef __u64 u64;
-typedef __u32 u32;
-typedef __u16 u16;
 
 #ifndef NUM_CPUS
 #define NUM_CPUS 128
@@ -50,7 +44,7 @@ typedef __u16 u16;
 #endif
 
 // irq counter, 10 is the max number of irq vectors
-#ifndef IRQ_MAX_LEN 
+#ifndef IRQ_MAX_LEN
 #define IRQ_MAX_LEN 10
 #endif
 
@@ -67,80 +61,132 @@ typedef __u16 u16;
 #endif
 
 // array size is to be reset in userspace
-#define BPF_ARRARY_MAP(_name, _type, _key_type, _value_type) \
-    struct {                                                 \
-        __uint(type, _type);                                 \
-        __type(key, _key_type);                              \
-        __type(value, _value_type);                          \
-        __uint(max_entries, NUM_CPUS);                       \
-    } _name SEC(".maps");
+#define BPF_ARRARY_MAP(_name, _type, _key_type, _value_type)                   \
+  struct {                                                                     \
+    __uint(type, _type);                                                       \
+    __type(key, _key_type);                                                    \
+    __type(value, _value_type);                                                \
+    __uint(max_entries, NUM_CPUS);                                             \
+  } _name SEC(".maps");
 
-#define BPF_HASH(_name, _key_type, _value_type) \
-    struct {                                    \
-        __uint(type, BPF_MAP_TYPE_HASH);        \
-        __type(key, _key_type);                 \
-        __type(value, _value_type);             \
-        __uint(max_entries, MAP_SIZE);          \
-    } _name SEC(".maps");
+#define BPF_HASH(_name, _key_type, _value_type)                                \
+  struct {                                                                     \
+    __uint(type, BPF_MAP_TYPE_HASH);                                           \
+    __type(key, _key_type);                                                    \
+    __type(value, _value_type);                                                \
+    __uint(max_entries, MAP_SIZE);                                             \
+  } _name SEC(".maps");
 
-#define BPF_ARRAY(_name, _leaf_type) \
-    BPF_ARRARY_MAP(_name, BPF_MAP_TYPE_ARRAY, u32, _leaf_type);
+#define BPF_ARRAY(_name, _leaf_type)                                           \
+  BPF_ARRARY_MAP(_name, BPF_MAP_TYPE_ARRAY, __u32, _leaf_type);
 
-#define BPF_PERF_ARRAY(_name) \
-    BPF_ARRARY_MAP(_name, BPF_MAP_TYPE_PERF_EVENT_ARRAY, int, u32)
+#define BPF_PERF_ARRAY(_name)                                                  \
+  BPF_ARRARY_MAP(_name, BPF_MAP_TYPE_PERF_EVENT_ARRAY, int, __u32)
 
 static __always_inline void *
-bpf_map_lookup_or_try_init(void *map, const void *key, const void *init)
-{
-	void *val;
-	int err;
+bpf_map_lookup_or_try_init(void *map, const void *key, const void *init) {
+  void *val;
+  int err;
 
-	val = bpf_map_lookup_elem(map, key);
-	if (val)
-		return val;
+  val = bpf_map_lookup_elem(map, key);
+  if (val)
+    return val;
 
-	err = bpf_map_update_elem(map, key, init, BPF_NOEXIST);
-	if (err && err != -17)
-		return 0;
+  err = bpf_map_update_elem(map, key, init, BPF_NOEXIST);
+  if (err && err != -17)
+    return 0;
 
-	return bpf_map_lookup_elem(map, key);
+  return bpf_map_lookup_elem(map, key);
 }
 
 struct sched_switch_args {
-    unsigned long long pad;
-    char prev_comm[TASK_COMM_LEN];
-    int prev_pid;
-    int prev_prio;
-    long long prev_state;
-    char next_comm[TASK_COMM_LEN];
-    int next_pid;
-    int next_prio;
+  unsigned long long pad;
+  char prev_comm[TASK_COMM_LEN];
+  int prev_pid;
+  int prev_prio;
+  long long prev_state;
+  char next_comm[TASK_COMM_LEN];
+  int next_pid;
+  int next_prio;
 };
 
-/*
 struct trace_event_raw_softirq {
-    unsigned long long pad;
-    unsigned int vec;
+  unsigned long long pad;
+  unsigned int vec;
 };
-*/
 
-typedef struct process_metrics_t
-{
-    u64 cgroup_id;
-    u64 pid; // pid is the kernel space view of the thread id
-    u64 tgid; // tgid is the user space view of the pid
-    u64 process_run_time;
-    u64 task_clock_time;
-    u64 cpu_cycles;
-    u64 cpu_instr;
-    u64 cache_miss;
-    u64 page_cache_hit;
-    u16 vec_nr[IRQ_MAX_LEN]; 
-    char comm[TASK_COMM_LEN];
+typedef struct process_metrics_t {
+  __u64 cgroup_id;
+  __u64 pid;  // pid is the kernel space view of the thread id
+  __u64 tgid; // tgid is the user space view of the pid
+  __u64 process_run_time;
+  __u64 task_clock_time;
+  __u64 cpu_cycles;
+  __u64 cpu_instr;
+  __u64 cache_miss;
+  __u64 page_cache_hit;
+  __u16 vec_nr[IRQ_MAX_LEN];
+  char comm[TASK_COMM_LEN];
 } process_metrics_t;
 
-typedef struct pid_time_t
-{
-    u32 pid;
+typedef struct pid_time_t {
+  __u32 pid;
 } pid_time_t;
 
+struct task_struct {
+  __u64 tgid;
+  __u32 pid;
+} __attribute__((preserve_access_index));
+
+#if defined(bpf_target_x86) // x86
+struct pt_regs {
+  unsigned long r15;
+  unsigned long r14;
+  unsigned long r13;
+  unsigned long r12;
+  unsigned long rbp;
+  unsigned long rbx;
+  unsigned long r11;
+  unsigned long r10;
+  unsigned long r9;
+  unsigned long r8;
+  unsigned long rax;
+  unsigned long rcx;
+  unsigned long rdx;
+  unsigned long rsi;
+  unsigned long rdi;
+  unsigned long orig_rax;
+  unsigned long rip;
+  unsigned long cs;
+  unsigned long eflags;
+  unsigned long rsp;
+  unsigned long ss;
+};
+#endif // defined(bpf_target_x86)
+
+#if defined(bpf_target_arm64) // arm64
+struct user_pt_regs {
+  __u64 regs[31];
+  __u64 sp;
+  __u64 pc;
+  __u64 pstate;
+};
+
+#endif // defined(bpf_target_arm64)
+
+#if defined(bpf_target_s390) // s390
+
+typedef struct {
+  unsigned long mask;
+  unsigned long addr;
+} __attribute__((aligned(8))) psw_t;
+
+typedef struct {
+  unsigned long args[1];
+  psw_t psw;
+  unsigned long gprs[16];
+} user_pt_regs;
+
+#endif // defined(bpf_target_s390)
+
+#endif // __KEPLER_H__
