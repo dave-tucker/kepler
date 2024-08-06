729b122491f31da01685ef448db62c6b6049826e
Dave Tucker <dave@dtucker.co.uk>
2024-08-06 10:37:24 +0100

    test(pkg/bpf): Add benchmark for PerCPUCounter
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- pkg/bpf/exporter_test.go
+++ pkg/bpf/exporter_test.go
@@ -1,12 +1,14 @@
 package bpf
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gmeasure"
 )
 
 var _ = Describe("PerCPUCounter", func() {
-
 	var counter PerCPUCounter
 	BeforeEach(func() {
 		counter = NewPerCPUCounter()
@@ -42,4 +44,18 @@ var _ = Describe("PerCPUCounter", func() {
 		Expect(counter.Total).To(Equal(uint64(0)))
 	})
 
+	It("should be as efficient as possible", func() {
+		experiment := gmeasure.NewExperiment("duration of start/stop")
+		AddReportEntry(experiment.Name, experiment)
+
+		experiment.Sample(func(idx int) {
+			experiment.MeasureDuration("duration of start/stop", func() {
+				counter.Start(1, uint32(idx), 10)
+				counter.Stop(1, uint32(idx), 21)
+			}, gmeasure.Precision(time.Nanosecond))
+		}, gmeasure.SamplingConfig{N: 1000, Duration: 10 * time.Second})
+
+		counter.Start(1, 1, 10)
+		Expect(counter.Values).To(HaveLen(1))
+	})
 })
